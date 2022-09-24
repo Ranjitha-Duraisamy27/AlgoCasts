f9b127cb1437ba6c44b57b4cffcc883a9219fa48
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-09-24 11:11:39 +0530

    reverse string exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Solution 1
+function reverse1(str) {
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    console.log(newStr);
+    return newStr;
+}
+
+module.exports = reverse;
+
+//solution 2
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+
 
 module.exports = reverse;
