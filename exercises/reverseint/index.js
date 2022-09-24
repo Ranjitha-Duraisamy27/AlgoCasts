f724c95e6ddc0ebdcddec3f39f5201080978e25b
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-09-24 13:01:31 +0530

    number reverse

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// solution 1
+function reverseInt1(n) {
+    const isNegative = n < 0;
+    let number = isNegative ? -(n) : n;
+    let reverseNum = '';
+    while (number > 0) {
+        reverseNum += number % 10;
+        number = Math.trunc(number / 10);
+    }
+    return isNegative ? -reverseNum : +reverseNum;
+}
+
+
+//solution 2
+function reverseInt(n) {
+    const sign = Math.sign(n);
+    const number = n * sign;
+    return +number.toString().split('').reverse().join('') * sign;
+}
+
+reverseInt(-51);
 
 module.exports = reverseInt;
