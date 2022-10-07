a74704e6a39c4b13253a22adc315fc657dd8f421
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-10-07 12:11:54 +0530

    solution for finding max occurrance of a letter

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+function maxChar(str) {
+    const chars = {};
+    for (const character of str) {
+        chars[character] = !chars[character] ? 1 : chars[character] + 1;
+    }
+    let max = 0;
+    let letter = '';
+    for (const [key, val] of Object.entries(chars)) {
+        if(max < val) {
+            max = val;
+            letter = key;
+        }
+    }
+    return letter;
+}
 
 module.exports = maxChar;
